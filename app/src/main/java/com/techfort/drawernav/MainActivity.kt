5e1ba43a18e1ac743c9937b4784a9355be5e4cb5
azizul <azizul@w3engineers.com>
2018-12-05 18:38:06 +0600

    drawer

--- app/src/main/java/com/techfort/drawernav/MainActivity.kt
+++ app/src/main/java/com/techfort/drawernav/MainActivity.kt
@@ -2,10 +2,12 @@ package com.techfort.drawernav
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.util.Log
 import androidx.core.view.GravityCompat
 import androidx.databinding.DataBindingUtil
 import androidx.drawerlayout.widget.DrawerLayout
 import androidx.navigation.NavController
+import androidx.navigation.NavDestination
 import androidx.navigation.Navigation
 import androidx.navigation.ui.AppBarConfiguration
 import androidx.navigation.ui.navigateUp
@@ -13,37 +15,42 @@ import androidx.navigation.ui.setupActionBarWithNavController
 import androidx.navigation.ui.setupWithNavController
 import com.techfort.drawernav.databinding.ActivityMainBinding
 
-class MainActivity : AppCompatActivity() {
+class MainActivity : AppCompatActivity(), NavController.OnNavigatedListener {
+
+
     private lateinit var drawerLayout: DrawerLayout
     private lateinit var appBarConfiguration: AppBarConfiguration
     private lateinit var navController: NavController
-
+    private val TAG = "MainActivity"
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        val binding: ActivityMainBinding = DataBindingUtil.setContentView(this,
-            R.layout.activity_main)
+        val binding: ActivityMainBinding = DataBindingUtil.setContentView(
+            this,
+            R.layout.activity_main
+        )
         drawerLayout = binding.drawerLayout
 
         navController = Navigation.findNavController(this, R.id.garden_nav_fragment)
         appBarConfiguration = AppBarConfiguration(navController.graph, drawerLayout)
 
         // Set up ActionBar
-       // setSupportActionBar(binding.toolbar)
+        setSupportActionBar(binding.toolbar)
         setupActionBarWithNavController(navController, appBarConfiguration)
 
         // Set up navigation menu
         binding.navigationView.setupWithNavController(navController)
+
+        navController.addOnNavigatedListener(this)
     }
 
     override fun onSupportNavigateUp(): Boolean {
+        Log.e(TAG, "onSupportNavigateUp()")
         return navController.navigateUp(appBarConfiguration) || super.onSupportNavigateUp()
     }
 
-    override fun onBackPressed() {
-        if (drawerLayout.isDrawerOpen(GravityCompat.START)) {
-            drawerLayout.closeDrawer(GravityCompat.START)
-        } else {
-            super.onBackPressed()
-        }
+
+    override fun onNavigated(controller: NavController, destination: NavDestination) {
+        Log.e(TAG, "onNavigated() ${destination.label}")
+        //supportActionBar!!.setDisplayHomeAsUpEnabled(false)
     }
 }
