7168b0c12172fabdaee2c23c2381962d7447fe20
azizul <azizul@w3engineers.com>
2018-12-05 18:16:52 +0600

    First commit

---
+++ app/src/main/java/com/techfort/drawernav/HomeFragment.kt
@@ -0,0 +1,28 @@
+package com.techfort.drawernav
+
+import android.os.Bundle
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import androidx.fragment.app.Fragment
+
+
+/*
+*  ****************************************************************************
+*  * Created by : Md. Azizul Islam on 12/5/2018 at 5:17 PM.
+*  * Email : azizul@w3engineers.com
+*  * 
+*  * Purpose:
+*  *
+*  * Last edited by : Md. Azizul Islam on 12/5/2018.
+*  * 
+*  * Last Reviewed by : <Reviewer Name> on <mm/dd/yy>  
+*  ****************************************************************************
+*/
+
+class HomeFragment : Fragment() {
+    override fun onCreateView(inflater: LayoutInflater, container: ViewGroup?, savedInstanceState: Bundle?): View? {
+        val view = inflater.inflate(R.layout.fragment_home, container, false)
+        return view
+    }
+}
---
+++ app/src/main/java/com/techfort/drawernav/MainActivity.kt
@@ -0,0 +1,49 @@
+package com.techfort.drawernav
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import androidx.core.view.GravityCompat
+import androidx.databinding.DataBindingUtil
+import androidx.drawerlayout.widget.DrawerLayout
+import androidx.navigation.NavController
+import androidx.navigation.Navigation
+import androidx.navigation.ui.AppBarConfiguration
+import androidx.navigation.ui.navigateUp
+import androidx.navigation.ui.setupActionBarWithNavController
+import androidx.navigation.ui.setupWithNavController
+import com.techfort.drawernav.databinding.ActivityMainBinding
+
+class MainActivity : AppCompatActivity() {
+    private lateinit var drawerLayout: DrawerLayout
+    private lateinit var appBarConfiguration: AppBarConfiguration
+    private lateinit var navController: NavController
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        val binding: ActivityMainBinding = DataBindingUtil.setContentView(this,
+            R.layout.activity_main)
+        drawerLayout = binding.drawerLayout
+
+        navController = Navigation.findNavController(this, R.id.garden_nav_fragment)
+        appBarConfiguration = AppBarConfiguration(navController.graph, drawerLayout)
+
+        // Set up ActionBar
+       // setSupportActionBar(binding.toolbar)
+        setupActionBarWithNavController(navController, appBarConfiguration)
+
+        // Set up navigation menu
+        binding.navigationView.setupWithNavController(navController)
+    }
+
+    override fun onSupportNavigateUp(): Boolean {
+        return navController.navigateUp(appBarConfiguration) || super.onSupportNavigateUp()
+    }
+
+    override fun onBackPressed() {
+        if (drawerLayout.isDrawerOpen(GravityCompat.START)) {
+            drawerLayout.closeDrawer(GravityCompat.START)
+        } else {
+            super.onBackPressed()
+        }
+    }
+}
